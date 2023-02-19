17f6d643554090ef5d94751b7fbc4b1fee205516
Douglas <douglascamposchagas2@gmail.com>
2023-02-19 09:28:31 -0300

    home structure added

--- app/src/main/java/com/example/whatsappclone_kotlin/ui/SplashActivity.kt
+++ app/src/main/java/com/example/whatsappclone_kotlin/ui/SplashActivity.kt
@@ -1,9 +1,10 @@
-package com.example.whatsappclone_kotlin
+package com.example.whatsappclone_kotlin.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.example.whatsappclone_kotlin.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
