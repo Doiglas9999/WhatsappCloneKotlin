5d96f26c27253547172c1a063c87a95a55df8828
Hilguener Silva <Hilgueneer@gmail.com>
2023-02-19 09:47:59 -0300

    splash screen added

--- app/src/main/java/com/example/whatsappclone_kotlin/SplashActivity.kt
+++ app/src/main/java/com/example/whatsappclone_kotlin/SplashActivity.kt
@@ -4,15 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.ActionBar
+import com.example.whatsappclone_kotlin.databinding.ActivitySplashBinding
+
+
+private lateinit var binding: ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
 
-        val intent = Intent (this, LoginActivity::class.java)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
+
+
+        val intent = Intent(this, LoginActivity::class.java)
         Handler().postDelayed({
             startActivity(intent)
-        },2500)
+            finish()
+        }, 2500)
     }
 }
